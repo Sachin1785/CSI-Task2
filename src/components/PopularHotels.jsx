74660c047076f74d6122ea947c3d40554a88e08b
Sachin1785 <sachhidanandamahapatro@gmail.com>
2024-08-23 00:05:22 +0530

    made it build ready

--- src/components/PopularHotels.jsx
+++ src/components/PopularHotels.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import HotelCard from './HotelCard';
+import hotel1 from "../assets/DJCSI Technical Tasks (1).png";
+import hotel2 from "../assets/DJCSI Technical Tasks (2).png";
+import hotel3 from "../assets/DJCSI Technical Tasks (3).png";
 
 function PopularHotels() {
     return (
@@ -10,19 +13,19 @@ function PopularHotels() {
                 <button className="view-all-btn">View All</button>
                 <div className="hotels-grid" style={{ display: 'flex', marginTop: '40px' }}>
                     <HotelCard
-                        imageSrc="src\assets\DJCSI Technical Tasks (1).png"
+                        imageSrc={hotel1}
                         name="Danubius Hotel Regents Park"
                         price="$200 Per Night"
                         rating={4}
                     />
                     <HotelCard
-                        imageSrc="src\assets\DJCSI Technical Tasks (2).png"
+                        imageSrc={hotel2}
                         name="The Resident Soho"
                         price="$200 Per Night"
                         rating={3.8}
                     />
                     <HotelCard
-                        imageSrc="src\assets\DJCSI Technical Tasks (3).png"
+                        imageSrc={hotel3}
                         name="London Bridge Hotel"
                         price="$200 Per Night"
                         rating={4.7}
